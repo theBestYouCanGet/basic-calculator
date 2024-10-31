267928211f75d68c6898aaa38c5a7ffb1b0baa90
theBestYouCanGet <akbimstafa02@gmail.com>
2024-10-31 23:20:18 +0100

    Added basic arithmetic oprations

--- calculator.py
+++ calculator.py
@@ -13,7 +13,8 @@ def divide(x,y):
     return x / y
 
 def main():
-    print("Welcome to your Basic Calculator;)")
+    print("Welcome to your Basic Calculator")
+    print("Select your operation: ")
     print("1. Addition")
     print("2. Substraction")
     print("3. Multiplication")
