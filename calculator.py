8030d5f50f6661e1471a3d9da6f5b4cd21da9263
theBestYouCanGet <akbimstafa02@gmail.com>
2024-10-31 23:52:02 +0100

    Fixed bug in division function

--- calculator.py
+++ calculator.py
@@ -14,34 +14,40 @@ def divide(x,y):
 
 def main():
     print("Welcome to your Basic Calculator")
-    print("Select your operation: ")
-    print("1. Addition")
-    print("2. Substraction")
-    print("3. Multiplication")
-    print("4. Division")
-
+    
     while True:
+        print("Select your operation: ")
+        print("1. Addition")
+        print("2. Substraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Exit")
+
         operation = input("Chose your Operation(1, 2, 3, 4): ")
 
         if operation in ['1', '2', '3', '4']:
-            x = float(input("Enter x value: "))
-            y = float(input("Enter y value: "))
+            try:
+                x = float(input("Enter x value: "))
+                y = float(input("Enter y value: "))
 
-            if operation == '1':
-                print("result: ", add(x, y))
-            elif operation == '2':
-                print("result: ", substract(x, y))
-            elif operation == '3':
-                print("result: ", multiply(x, y))
-            elif operatio == '4':
-                print("result: ", divide(x, y))
+                if operation == '1':
+                    print("result: ", add(x, y))
+                elif operation == '2':
+                    print("result: ", substract(x, y))
+                elif operation == '3':
+                    print("result: ", multiply(x, y))
+                elif operatio == '4':
+                    print("result: ", divide(x, y))
+        
+            except ValueErroe:
+                print("Invalid input! Please enter numbers only.")
+        
+        elif operation == '5':
+            print("Exiting the calculator. Goodbye! ")
+            break
         else:
             print("Invalid choice")
 
-
-            next_operation = input("do you want to perform another operation? (yes/no): ")
-            if next_operation.lower() != 'yes':
-                break
 
 if __name__ == "__main__":
     main()
